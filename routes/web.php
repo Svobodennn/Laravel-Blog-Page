8976d0a9306ba47f5a1093a020cf0d5cce5672ad
Svobodennn <melih.sarac@hotmail.com>
2023-11-20 22:20:05 +0300

    Homepage route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('front.homepage');
+//});
+
+Route::get('/', 'App\Http\Controllers\Front\Homepage@index')->name('homepage');
