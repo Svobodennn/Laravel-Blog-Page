0c30b3e1a0c048d6cb8c54e710eeba6c1ce35899
Svobodennn <melih.sarac@hotmail.com>
2023-11-20 22:20:51 +0300

    Categories and Articles migrations

---
+++ database/migrations/2023_11_20_105734_articles.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id'); // to make it relational in laravel integer('name')->unsigned()
+            $table->string('title');
+            $table->string('image');
+            $table->longText('content');
+            $table->integer('hit')->default(0);
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
