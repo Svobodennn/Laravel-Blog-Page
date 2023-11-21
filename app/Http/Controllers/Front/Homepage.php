6f5b6bd3c95d344e730c762c19fe29ecc5a038e6
Svobodennn <melih.sarac@hotmail.com>
2023-11-21 11:04:32 +0300

    Added function to view article. Checks if category exists and checks if article in this category and inrements hit count

--- app/Http/Controllers/Front/Homepage.php
+++ app/Http/Controllers/Front/Homepage.php
@@ -16,4 +16,14 @@ class Homepage extends Controller
         $data['categories'] = Category::inRandomOrder()->get();
         return view('front.homepage', $data);
     }
+
+    public function single($category,$slug){
+        $category=Category::where('slug',$category)->first() ?? abort (404); // whereSlug($slug)
+        $article=Article::where('slug',$slug)->whereCategoryId($category->id)->first() ?? abort (404);
+
+        $article->increment('hit');
+        $data['article'] = $article;
+        $data['categories'] = Category::inRandomOrder()->get();
+        return view('front.single',$data);
+    }
 }
