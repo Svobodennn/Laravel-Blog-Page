4cb856bcb7517c51e7aa5f9970f112c00556eeac
Svobodennn <melih.sarac@hotmail.com>
2023-11-23 23:31:40 +0300

    contact post and routing

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\Front\Homepage@index')->name('homepage');
 Route::get('/articles', 'App\Http\Controllers\Front\Homepage@index');
+Route::get('/contact','App\Http\Controllers\Front\Homepage@contact')->name('contact'); // Fixed defined URLs should be placed on top
+Route::post('/contact','App\Http\Controllers\Front\Homepage@contactPost')->name('contact.post');
 Route::get('/category/{category}','App\Http\Controllers\Front\Homepage@category')->name('category');
 Route::get('/{category}/{slug}','App\Http\Controllers\Front\Homepage@single')->name('single');
 Route::get('/{page}','App\Http\Controllers\Front\Homepage@page')->name('page');
