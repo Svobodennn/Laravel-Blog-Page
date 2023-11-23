3ece182318f81c829cfd645f851d8e7091c49d71
Svobodennn <melih.sarac@hotmail.com>
2023-11-23 12:05:03 +0300

    category view function

--- app/Http/Controllers/Front/Homepage.php
+++ app/Http/Controllers/Front/Homepage.php
@@ -13,7 +13,7 @@ class Homepage extends Controller
 {
     public function index(){
         $data['articles'] = Article::orderBy('created_at','DESC')->get();
-        $data['categories'] = Category::inRandomOrder()->get();
+        $data['categories'] = Category::orderBy('id','ASC')->get();
         return view('front.homepage', $data);
     }
 
@@ -23,7 +23,16 @@ class Homepage extends Controller
 
         $article->increment('hit');
         $data['article'] = $article;
-        $data['categories'] = Category::inRandomOrder()->get();
+        $data['categories'] = Category::orderBy('id','ASC')->get();
         return view('front.single',$data);
+    }
+
+    public function category($slug){
+        $category=Category::where('slug',$slug)->first() ?? abort (404,"This category doesn't exist");
+        $data['category'] = $category;
+        $data['articles'] = Article::where('category_id',$category->id)->orderBy('created_at','DESC')->get();
+        $data['categories'] = Category::orderBy('id','ASC')->get();
+
+        return view('front.category',$data);
     }
 }
