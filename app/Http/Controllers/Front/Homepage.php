52e517ccf5b4095696fa5f63520c525768a86f44
Svobodennn <melih.sarac@hotmail.com>
2023-11-23 21:57:20 +0300

    added constructor method to show categories and pages on all pages

--- app/Http/Controllers/Front/Homepage.php
+++ app/Http/Controllers/Front/Homepage.php
@@ -5,26 +5,36 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Database\Seeders\ArticleSeeder;
 use Illuminate\Http\Request;
+use App\Models\Page;
 
 // Models
 use App\Models\Category;
 use App\Models\Article;
 class Homepage extends Controller
 {
+    public function __construct()
+    {
+        view()->share('pages',Page::orderBy('order','ASC')->get()); // to share with all the functions within the controller
+        view()->share('categories',Category::orderBy('id','ASC')->get());
+    }
+
     public function index(){
         $data['articles'] = Article::orderBy('created_at','DESC')->paginate(2);
         $data['articles']->withPath(url('articles'));
-        $data['categories'] = Category::orderBy('id','ASC')->get();
+//        $data['categories'] = Category::orderBy('id','ASC')->get();
+//        $data['pages'] = Page::orderBy('order','ASC')->get();
         return view('front.homepage', $data);
     }
 
     public function single($category,$slug){
         $category=Category::where('slug',$category)->first() ?? abort (404); // whereSlug($slug)
         $article=Article::where('slug',$slug)->whereCategoryId($category->id)->first() ?? abort (404);
+//        $data['pages'] = Page::orderBy('order','ASC')->get();
+
 
         $article->increment('hit');
         $data['article'] = $article;
-        $data['categories'] = Category::orderBy('id','ASC')->get();
+//        $data['categories'] = Category::orderBy('id','ASC')->get();
         return view('front.single',$data);
     }
 
@@ -32,8 +42,19 @@ class Homepage extends Controller
         $category=Category::where('slug',$slug)->first() ?? abort (404,"This category doesn't exist");
         $data['category'] = $category;
         $data['articles'] = Article::where('category_id',$category->id)->orderBy('created_at','DESC')->paginate(2);
-        $data['categories'] = Category::orderBy('id','ASC')->get();
+//        $data['categories'] = Category::orderBy('id','ASC')->get();
+//        $data['pages'] = Page::orderBy('order','ASC')->get();
+
 
         return view('front.category',$data);
+    }
+
+    public function page($slug){
+        $page=Page::where('slug',$slug)->first() ?? abort(404);
+        $data['page'] = $page;
+//        $data['pages'] = Page::orderBy('order','ASC')->get();
+
+
+        return view('front.page',$data);
     }
 }
