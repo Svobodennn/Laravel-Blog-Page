b152cd9a875c202b63d9199a128ca9daa386a08f
Svobodennn <melih.sarac@hotmail.com>
2023-11-28 18:07:07 +0300

    login and logout functions using Auth class

--- app/Http/Controllers/Back/Auth.php
+++ app/Http/Controllers/Back/Auth.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as Authenticate;
+
+//auth class
 
 class Auth extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view('back.auth.login');
+    }
+
+    public function loginPost(Request $request)
+    {
+//        dd($request->post()); dump and die
+        if (Authenticate::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            return redirect()->route('dashboard');
+        }
+
+        return redirect()->route('login')->withErrors('Email or password is wrong');
+
+    }
+
+    public function logout(){
+        Authenticate::logout();
+        return redirect()->route('login');
     }
 }
