9b818c026d8c351ba6f1f10d3bfb14aee810ce3f
Svobodennn <melih.sarac@hotmail.com>
2023-11-28 16:42:43 +0300

    Admin panel login and dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 //Route::get('/', function () {
 //    return view('front.homepage');
 //});
+
+/*
+|--------------------------------------------------------------------------
+| Backend routes
+|--------------------------------------------------------------------------
+*/
+Route::get('admin/panel', 'App\Http\Controllers\Back\Dashboard@index')->name('dashboard');
+Route::get('admin/login', 'App\Http\Controllers\Back\Auth@login')->name('login');
+
+
+/*
+|--------------------------------------------------------------------------
+| Front routes
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', 'App\Http\Controllers\Front\Homepage@index')->name('homepage');
 Route::get('/articles', 'App\Http\Controllers\Front\Homepage@index');
@@ -24,3 +30,4 @@ Route::post('/contact','App\Http\Controllers\Front\Homepage@contactPost')->name(
 Route::get('/category/{category}','App\Http\Controllers\Front\Homepage@category')->name('category');
 Route::get('/{category}/{slug}','App\Http\Controllers\Front\Homepage@single')->name('single');
 Route::get('/{page}','App\Http\Controllers\Front\Homepage@page')->name('page');
+
