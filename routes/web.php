2bcdbe7b326827de10aefb6663b5b201670de0a7
Svobodennn <melih.sarac@hotmail.com>
2023-11-29 16:26:19 +0300

    Backend routes gropued with prefix and added middleware controlling

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Back\Auth;
+use App\Http\Controllers\Back\Dashboard;
+use App\Http\Controllers\Front\Homepage;
 
 
 //Route::get('/', function () {
@@ -13,10 +15,27 @@ use Illuminate\Support\Facades\Route;
 | Backend routes
 |--------------------------------------------------------------------------
 */
-Route::get('admin/panel', 'App\Http\Controllers\Back\Dashboard@index')->name('dashboard');
-Route::get('admin/login', 'App\Http\Controllers\Back\Auth@login')->name('login');
-Route::post('admin/login', 'App\Http\Controllers\Back\Auth@loginPost')->name('login.post');
-Route::get('admin/logout','App\Http\Controllers\Back\Auth@logout')->name('logout');
+
+
+
+Route::prefix('admin')->group(function (){
+//    $back = "App\Http\Controllers\Back";
+    Route::middleware(['isLogin'])->group(function (){
+//        global $back;
+        Route::get('/login',[Auth::class,'login'])->name('login');
+        Route::post('/login',[Auth::class,'loginPost'])->name('login.post');
+//        Route::get('/login', $back .'\Auth@login')->name('login');
+//        Route::post('/login', $back . '\Auth@loginPost')->name('login.post');
+    });
+
+    Route::middleware(['isAdmin'])->group(function (){
+//        global $back;
+        Route::get('/panel', [Dashboard::class, 'index'])->name('dashboard');
+        Route::get('/logout',[Auth::class, 'logout'])->name('logout');
+//        Route::get('/panel', '\Dashboard@index')->name('dashboard');
+//        Route::get('/logout', $back . '\Auth@logout')->name('logout');
+    });
+});
 
 
 /*
@@ -25,11 +44,12 @@ Route::get('admin/logout','App\Http\Controllers\Back\Auth@logout')->name('logout
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', 'App\Http\Controllers\Front\Homepage@index')->name('homepage');
-Route::get('/articles', 'App\Http\Controllers\Front\Homepage@index');
-Route::get('/contact','App\Http\Controllers\Front\Homepage@contact')->name('contact'); // Fixed defined URLs should be placed on top
-Route::post('/contact','App\Http\Controllers\Front\Homepage@contactPost')->name('contact.post');
-Route::get('/category/{category}','App\Http\Controllers\Front\Homepage@category')->name('category');
-Route::get('/{category}/{slug}','App\Http\Controllers\Front\Homepage@single')->name('single');
-Route::get('/{page}','App\Http\Controllers\Front\Homepage@page')->name('page');
+$front = "App\Http\Controllers\Front";
+Route::get('/', $front . '\Homepage@index')->name('homepage');
+Route::get('/articles', $front . '\Homepage@index');
+Route::get('/contact', $front . '\Homepage@contact')->name('contact'); // Fixed defined URLs should be placed on top
+Route::post('/contact', $front . '\Homepage@contactPost')->name('contact.post');
+Route::get('/category/{category}', $front . '\Homepage@category')->name('category');
+Route::get('/{category}/{slug}', $front . '\Homepage@single')->name('single');
+Route::get('/{page}', $front . '\Homepage@page')->name('page');
 
