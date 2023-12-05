359de0fad3c153f51cba2da7792ad12259cd7c5f
Svobodennn <melih.sarac@hotmail.com>
2023-12-05 11:33:56 +0300

    Added soft delete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function articleCount(){
         return $this->hasMany('App\Models\Article','category_id','id')->count();
     }
+
+
 }
